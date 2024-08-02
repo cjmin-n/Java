5b58ea250e4cec3e4e3fec86fa324bdfa1c981f8
jeongmin <mymemose@naver.com>
2024-08-02 17:36:26 +0900

    chap2

--- chap02-operator/src/main/java/com/ohgiraffers/section02/assignment/Application.java
+++ chap02-operator/src/main/java/com/ohgiraffers/section02/assignment/Application.java
@@ -14,5 +14,24 @@ public class Application {
         * '%=' : 왼쪽의 피 연산자에 오른쪽의 피 연산자를 나눈 나머지 결과를 대입한다.
         * */
 
+        int num = 12;
+        System.out.println("num = " + num); // soutv
+        
+        // 3 증가
+        num = num + 3;
+        System.out.println("num = " + num);
+
+        num += 3;
+        System.out.println("num = " + num);
+        
+        // 5로 나눈 나머지 값
+        num %= 5;
+        System.out.println("num = " + num);
+
+        num = 12;
+        //num = num * 4.1; 에러. 값이 실수로 나오기 때문에.
+        num *= 4.1; // 복합대입연산자를 사용하면 형변환도 해준다.
+        System.out.println("num = " + num);
+        
     }
 }
