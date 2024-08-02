093f337722ac625923ae5a4de0667048454d0d6d
jeongmin <mymemose@naver.com>
2024-08-02 17:39:35 +0900

    chap1 주석추가

--- chap01-literal-and-variable/src/section01/literal/Application02.java
+++ chap01-literal-and-variable/src/section01/literal/Application02.java
@@ -34,16 +34,16 @@ public class Application02 {
            컴퓨터는 문자를 인식하지 못하기 때문에 대응되는 숫자들이 있다.
            아스키 코드표(영어) / 유니코드 (전세계 언어)
          */
-        System.out.println('a'+'b');
-        System.out.println('ㄱ'+'ㄴ');
+        System.out.println('a'+'b'); // 195
+        System.out.println('ㄱ'+'ㄴ'); // 25189
 
         // 문자열은 + 연산만 가능하다.
         // 데이터 값이 다를 때는 자동 형변환을 통해 최상위 타입으로 변경된다.
         // (문자열이 최상위 타입)
         System.out.println("------- 문자 열과 다른 형태의 값 연산 -------");
 
-        System.out.println("hello" + 1004);
-        System.out.println("hello" + true);
+        System.out.println("hello" + 1004); // hello1004
+        System.out.println("hello" + true); // hellotrue
 
         System.out.println("hello" + 123 + 456 + "world"); //hello123456world
         System.out.println("hello" + ( 123 + 456 ) + "world"); //hello579world
