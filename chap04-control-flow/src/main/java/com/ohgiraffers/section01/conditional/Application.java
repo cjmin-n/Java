b94da0f4f137f1bf14ead705ed151fee4f15d8d0
jeongmin <mymemose@naver.com>
2024-08-07 10:29:32 +0900

    upload

---
+++ chap04-control-flow/src/main/java/com/ohgiraffers/section01/conditional/Application.java
@@ -0,0 +1,11 @@
+package main.java.com.ohgiraffers.section01.conditional;
+
+public class Application {
+    public static void main(String[] args) {
+
+        A_if a = new A_if();
+        // a.testSimpleIfStatement();
+        a.testNestedIfStatement();
+
+    }
+}
