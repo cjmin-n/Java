c24e62b7377942ae18e822b38f872f2417dc312f
cjmin-n <mymemose@naver.com>
2024-08-08 23:45:39 +0900

    question upload

--- chap05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/Question.java
+++ chap05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/Question.java
@@ -9,29 +9,52 @@ public class Question {
 
         // 트럼프 카드를 랜덤으로 한장 출력해보자.
 
+        // 1. num 배열 일일이 써준 경우
         String[] shape = {"다이아", "하트", "스페이드", "클로버"};
-        // int[] num = new int[10];
-        String[] num = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-
-        /*for (int i = 0; i < num.length; i++) {
-            num[i] = i + 1;
-        }*/
+        String[] num = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
         int random = (int)(Math.random()*3)+1;
-        //int random2 = (int)(Math.random()*9)+1;
         int random2 = (int)(Math.random()*12)+1;
         System.out.println("트럼프 카드 랜덤 한장 출력 : " + shape[random] + " " + num[random2]);
 
-        int[] num1 = new int[10];
+        System.out.println("================================");
+
+        // 2. num 배열 합친 경우
+        int[] num1 = new int[9];
         for (int i = 0; i < num1.length; i++) {
-            num1[i] = i + 1;
-        }
+            num1[i] = i + 2;
+        } // -> int[] num1 = {2, 3, 4, 5, 6, 7, 8, 9, 10}
+
         String[] num2 = {"A", "J", "Q", "K"};
-        int totalLength = num1.length + num2.length;
-        for (int j = 0; j < totalLength; j++) {
 
+        int totalLength = num1.length + num2.length;
+        String[] totalNum = new String[totalLength];
+
+        for (int j = 0; j < totalLength; j++) {
+            if( j == 0 ) {
+                totalNum[0]= num2[0];
+            } else if(j > 0 && j < 10){
+                totalNum[j] = String.valueOf(num1[j-1]);
+            } else {
+                int su = totalLength - j;
+                totalNum[j] = num2[num2.length-su];
+                for (int k = 1; k < num2.length ; k++) {
+                    if (su == k){
+                        break;
+                    }
+                }
+            }
+            /*else if (j == 10){ // total.length-3
+                totalNum[10] = num2[num2.length-3];
+            }else if(j == 11){
+                totalNum[11] = num2[num2.length-2];
+            }else if(j == 12){ //total.length-2
+                totalNum[12] = num2[num2.length-1];
+            }*/
         }
 
+        System.out.println(Arrays.toString(totalNum));
+        System.out.println("트럼프 카드 랜덤 한장 출력2 : " + shape[random] + " " + totalNum[random2]);
 
         /*
          *  배열의 길이와 배열의 요소들을 스캐너를 통해 입력받아 정수형 배열을 만들고, -> 먼저 길이를 받고 if 문 으로 배열 길이 1이하 조건 넣기/ 길이만큼 숫자받는거 반복
@@ -61,29 +84,19 @@ public class Question {
             System.out.println("배열의 요소를 입력 해 주세요 : ");
             arrNum = scr.nextInt();
             arr[i] = arrNum;
-            max = Math.max(max, arrNum);
+            // 2 3 5
+            max = Math.max(max, arrNum); // 5
 
-            /*if(max > maxSecond && maxSecond > arrNum){
-                maxSecond = max;
-            }*/
-            /*if (max > maxSecond){
+            if (max > arrNum){
                 maxSecond = Math.max(maxSecond, arrNum);
-            }*/
+            }
 
         }
 
+        System.out.println("정수형 배열 출력" + Arrays.toString(arr));
 
         System.out.println("만들어진 값 중에 최고 값 : " + max);
         System.out.println("만들어진 값 중에 두번째 최고 값 : " + maxSecond);
-
-
-        System.out.println(Arrays.toString(arr));
-
-
-
-
-
-
 
 
     }
