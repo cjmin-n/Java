d1b70dd3a6bd4666847f9e84c215df9852a56fd1
jeongmin <mymemose@naver.com>
2024-08-12 17:38:04 +0900

    다양한 방법으로 정답

--- chap06-class-and-object-lecture/src/main/java/com/ohgiraffers/section06/Question/Application.java
+++ chap06-class-and-object-lecture/src/main/java/com/ohgiraffers/section06/Question/Application.java
@@ -9,21 +9,33 @@ public class Application {
         BookDTO book = new BookDTO();
         book.printInformation();
 
-        BookDTO book2 = new BookDTO();
+        BookDTO book2 = new BookDTO("자바의 정석", "도우출판", "남궁성");
+
+        /*BookDTO book2 = new BookDTO();
 
         book2.setTitle("자바의 정석");
         book2.setPublisher("도우출판");
-        book2.setAuthor("남궁성");
+        book2.setAuthor("남궁성");*/
 
         book2.printInformation();
 
         BookDTO book3 = new BookDTO();
+
         book3.setTitle("홍길동전");
         book3.setPublisher("활빈당");
         book3.setAuthor("허균");
         book3.setPrice(5000000);
         book3.setDiscountRate(0.5);
-        book3.printInformation();
+
+        //book3.printInformation();
+        System.out.println(book3.getTitle() + " " + book3.getPublisher() + " " + book3.getAuthor() + " " + book3.getPrice() + " " + book3.getDiscountRate());
+
+
+        /*
+        null null null 0 0.0
+        자바의 정석 도우출판 남궁성 0 0.0
+        홍길동전 활빈당 허균 5000000 0.5
+        * */
 
     }
 }
